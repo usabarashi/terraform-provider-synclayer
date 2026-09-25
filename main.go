c1ede7d19c1c3bd0b812c5067608ecaf3e498cef
usabarashi <19676305+usabarashi@users.noreply.github.com>
2026-09-25 09:10:39 +0900

    Implement release pipeline and update workflows for Terraform provider

--- main.go
+++ main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
 	"github.com/usabarashi/terraform-provider-synclayer/internal/provider"
 )
 
+// version is set at build time via -ldflags "-X main.version=...".
+var version = "dev"
+
 func main() {
-	var debug bool
+	var (
+		debug       bool
+		showVersion bool
+	)
 
 	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.BoolVar(&showVersion, "version", false, "print the provider version and exit")
 	flag.Parse()
+
+	if showVersion {
+		fmt.Println(version)
+		return
+	}
 
 	plugin.Serve(&plugin.ServeOpts{
 		Debug:        debug,
